ec4355753d6a2375c7415e7a7159277a4522d22e
charles.ma <charles.ma@HHP-CHARLES.sidiamao.com.br>
2015-11-12 17:48:22 -0400

    Subindo a configurado do JMS

--- church-project/church-model/src/main/java/com/church/model/applicationConf/ApplicationConfig.java
+++ church-project/church-model/src/main/java/com/church/model/applicationConf/ApplicationConfig.java
@@ -1,10 +1,22 @@
 package com.church.model.applicationConf;
 
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSSecurityException;
+import javax.jms.Topic;
+
+import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @ImportResource({"classpath*:META-INF/spring/applicationContext-model.xml" })
@@ -16,8 +28,49 @@ import org.springframework.context.annotation.PropertySources;
 @Import({ PersistenceContext.class })
 public class ApplicationConfig {
 
-
+	private static final String PROPERTY_NAME_ACTIVEMQ_URL = "activemq.url";
+	private static final String PROPERTY_NAME_ACTIVEMQ_APPLI_TOPIC = "activemq.appli.topic";
+	private static final String PROPERTY_NAME_ACTIVEMQ_MIMPOOLSIZE = "activemq.minPoolSize";
+	private static final String PROPERTY_NAME_ACTIVEMQ_MAXPOOLSIZE = "activemq.maxPoolSize";
+	private static final String PROPERTY_NAME_ACTIVEMQ_SEND_TIMEOUT = "activemq.sendtimeout";
+	
+	@Resource
+	private Environment env;
 	
 	
-
+	@Bean
+	public ConnectionFactory jmsConnectionFactory(){
+		
+		//Get the reference the connection factory
+		final ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+		
+		//Configure factory
+		factory.setBrokerURL(env.getRequiredProperty(PROPERTY_NAME_ACTIVEMQ_URL));
+		factory.setSendTimeout(Integer.parseInt(env.getRequiredProperty(PROPERTY_NAME_ACTIVEMQ_SEND_TIMEOUT)));
+		factory.setUserName("root");
+		factory.setPassword("root1234");
+		
+		return new PooledConnectionFactory(factory);
+	}
+	
+	@Bean
+	public Topic electrictyTopic(){
+		
+		return new ActiveMQTopic(env.getRequiredProperty(PROPERTY_NAME_ACTIVEMQ_APPLI_TOPIC));
+	}
+	
+	@Bean
+	public JmsOperations jmsTemplateTopic(){
+		
+		//Define the JmsTemplate
+		final JmsTemplate jmsTemplate = new JmsTemplate(jmsConnectionFactory());
+		jmsTemplate.setDefaultDestination(electrictyTopic());
+		jmsTemplate.setReceiveTimeout(Integer.parseInt(env.getRequiredProperty(PROPERTY_NAME_ACTIVEMQ_SEND_TIMEOUT)));
+		jmsTemplate.setSessionTransacted(true);
+		jmsTemplate.setExplicitQosEnabled(true);
+		return jmsTemplate;
+	}
+	
+	
+	
 }
